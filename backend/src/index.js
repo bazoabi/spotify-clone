818622319c15ca25601dd4ebef7a456f339a4540
Bader Zoabi <bader.zoabi@gmail.com>
2025-05-24 21:31:11 +0200

    Add Backend Routes Structure

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,11 +2,28 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
+import userRoutes from "./routes/user.route.js";
+import adminRoutes from "./routes/admin.route.js";
+import authRoutes from "./routes/auth.route.js";
+import songRoutes from "./routes/song.route.js";
+import albumRoutes from "./routes/album.route.js";
+import statRoutes from "./routes/stat.route.js";
+
 // Load environment variables from .env file
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/admin", adminRoutes);
+app.use("/api/songs", songRoutes);
+app.use("/api/albums", albumRoutes);
+app.use("/api/stats", statRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
