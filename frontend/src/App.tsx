a0aa519d6b5e06d406def53cae3699d0173e9198
Bader Zoabi <bader.zoabi@gmail.com>
2025-05-24 20:35:38 +0200

    Add Clerk Auth To Frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,24 @@
 import { Button } from "./components/ui/button";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
 
 function App() {
   return (
     <>
-      <h1 className="text-red-500 text-5xl">Hello</h1>
-      <Button variant="destructive">Click me</Button>
+      <header>
+        <SignedOut>
+          <SignInButton>
+            <Button variant="default">Sign In</Button>
+          </SignInButton>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </header>
     </>
   );
 }
